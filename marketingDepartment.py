f2eafcc001579ad4915f6f38849bc3595367e5b6
calebwatt15@gmail.com <calebwatt15@gmail.com>
2014-03-09 03:25:14 -0400

    removed duplicate 'integration'. now supports python 2 and 3.

--- marketingDepartment.py
+++ marketingDepartment.py
@@ -3,8 +3,9 @@
 #CYBER CYBER!!
 import random
 import os
+import sys
 
-bar = ['cyber', 'cloud', 'threat', 'analytics', 'infrastructure', 'software-as-a-service', 'virtualization', 'distributed', 'denial', 'critical', 'mission', 'mobile', 'platform', 'intensive', 'application', 'intelligence', 'architecture', 'data', 'gathering', 'information', 'system', 'end-user', 'support', 'cross', 'fault', 'security', 'BYOD', 'defense', 'license', 'multiplatform', 'integration', 'network', 'admin', 'reverse engineering', 'testing', 'product', 'global', 'password', 'management', 'research', 'development', 'root', 'software', 'hardware', 'kernel', 'exploit', 'proprietary', 'server', 'cyberbullying', 'VOiP', 'web-based', 'front-end', 'workflow', 'optimization', 'innovative', 'specialized', 'attack-vector', 'professional', 'leader', 'forensics', 'occupational', 'hashtag', 'social', 'integration', 'USCC']
+bar = ['cyber', 'cloud', 'threat', 'analytics', 'infrastructure', 'software-as-a-service', 'virtualization', 'distributed', 'denial', 'critical', 'mission', 'mobile', 'platform', 'intensive', 'application', 'intelligence', 'architecture', 'data', 'gathering', 'information', 'system', 'end-user', 'support', 'cross', 'fault', 'security', 'BYOD', 'defense', 'license', 'multiplatform', 'integration', 'network', 'admin', 'reverse engineering', 'testing', 'product', 'global', 'password', 'management', 'research', 'development', 'root', 'software', 'hardware', 'kernel', 'exploit', 'proprietary', 'server', 'cyberbullying', 'VOiP', 'web-based', 'front-end', 'workflow', 'optimization', 'innovative', 'specialized', 'attack-vector', 'professional', 'leader', 'forensics', 'occupational', 'hashtag', 'social']
 
 bzwd = len(bar) - 1
 
@@ -14,6 +15,10 @@ while(True):
 	while(b==a and b!=0):
 		b = random.randint(0,bzwd)
 	os.system("clear")
-	print bar[a] + " " + bar[b]
-	print "Press enter for a new buzzword pair."
-	xyz = raw_input()
+	print(bar[a] + " " + bar[b])
+	print("Press enter for a new buzzword pair.")
+	pyVersion = sys.version[0]
+	if(int(pyVersion) < 3):
+		xyz = raw_input()
+	else:
+		xyz = input()
