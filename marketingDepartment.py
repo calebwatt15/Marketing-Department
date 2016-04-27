fffe9a4b27fe99cb23b44ed3505338d1a821afb5
Caleb Watt <calebwatt15@gmail.com>
2016-04-27 08:55:43 -0500

    Functionality moved to functions, removed cyber-cyber possibility

--- marketingDepartment.py
+++ marketingDepartment.py
@@ -1,27 +1,36 @@
 #!/usr/bin/python
 #Developed by Caleb Watt and Ben Donnelly for USCC Western Regional 2013
-#CYBER CYBER!!
+#CYBER THREAT-HUNT!!
 import random
 import os
 import sys
 
 buzzwords = ['cyber', 'cloud', 'threat', 'analytics', 'infrastructure', 'software-as-a-service', 'virtualization', 'distributed', 'denial', 'critical', 'mission', 'mobile', 'platform', 'intensive', 'application', 'intelligence', 'architecture', 'data', 'gathering', 'information', 'system', 'end-user', 'support', 'cross', 'fault', 'security', 'BYOD', 'defense', 'license', 'multiplatform', 'integration', 'network', 'admin', 'reverse engineering', 'testing', 'product', 'global', 'password', 'management', 'research', 'development', 'root', 'software', 'hardware', 'kernel', 'exploit', 'proprietary', 'server', 'cyberbullying', 'VOiP', 'web-based', 'front-end', 'workflow', 'optimization', 'innovative', 'specialized', 'attack-vector', 'professional', 'leader', 'forensics', 'occupational', 'hashtag', 'social']
 
-numBuzzwords = len(buzzwords) - 1
 
-while(True):
-	a = random.randint(0,numBuzzwords)
-	b = a
-	while(b==a and b!=0):
-		b = random.randint(0,numBuzzwords)
+def getBuzzwords(maxWord):
+	firstWord = random.randint(0,maxWord)
+	secondWord = random.randint(0,maxWord)
+	while(firstWord==secondWord):
+		secondWord = random.randint(0,maxWord)
+	buzzwordPair = buzzwords[firstWord] + " " + buzzwords[secondWord]
+	return(buzzwordPair)
+
+def clearScreen():
 	if(sys.platform[:3]=='win'):
 		os.system("cls")
 	else:
 		os.system("clear")
-	print(buzzwords[a] + " " + buzzwords[b])
+
+def printBuzzwords(wordString):
+	print(wordString)
 	print("Press enter for a new buzzword pair.")
 	pyVersion = sys.version[0]
 	if(int(pyVersion) < 3):
-		xyz = raw_input()
+		keyPress = raw_input()
 	else:
-		xyz = input()
+		keyPress = input()
+		
+while(True):
+	clearScreen()	
+	printBuzzwords(getBuzzwords(len(buzzwords) - 1))
