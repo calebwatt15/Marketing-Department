23a04c63bdcbfdbbc1d8e976466c1dc39640f762
Caleb Watt <calebwatt15@gmail.com>
2016-04-27 08:38:35 -0500

    Fixed variable names

--- marketingDepartment.py
+++ marketingDepartment.py
@@ -5,20 +5,20 @@ import random
 import os
 import sys
 
-bar = ['cyber', 'cloud', 'threat', 'analytics', 'infrastructure', 'software-as-a-service', 'virtualization', 'distributed', 'denial', 'critical', 'mission', 'mobile', 'platform', 'intensive', 'application', 'intelligence', 'architecture', 'data', 'gathering', 'information', 'system', 'end-user', 'support', 'cross', 'fault', 'security', 'BYOD', 'defense', 'license', 'multiplatform', 'integration', 'network', 'admin', 'reverse engineering', 'testing', 'product', 'global', 'password', 'management', 'research', 'development', 'root', 'software', 'hardware', 'kernel', 'exploit', 'proprietary', 'server', 'cyberbullying', 'VOiP', 'web-based', 'front-end', 'workflow', 'optimization', 'innovative', 'specialized', 'attack-vector', 'professional', 'leader', 'forensics', 'occupational', 'hashtag', 'social']
+buzzwords = ['cyber', 'cloud', 'threat', 'analytics', 'infrastructure', 'software-as-a-service', 'virtualization', 'distributed', 'denial', 'critical', 'mission', 'mobile', 'platform', 'intensive', 'application', 'intelligence', 'architecture', 'data', 'gathering', 'information', 'system', 'end-user', 'support', 'cross', 'fault', 'security', 'BYOD', 'defense', 'license', 'multiplatform', 'integration', 'network', 'admin', 'reverse engineering', 'testing', 'product', 'global', 'password', 'management', 'research', 'development', 'root', 'software', 'hardware', 'kernel', 'exploit', 'proprietary', 'server', 'cyberbullying', 'VOiP', 'web-based', 'front-end', 'workflow', 'optimization', 'innovative', 'specialized', 'attack-vector', 'professional', 'leader', 'forensics', 'occupational', 'hashtag', 'social']
 
-bzwd = len(bar) - 1
+numBuzzwords = len(buzzwords) - 1
 
 while(True):
-	a = random.randint(0,bzwd)
+	a = random.randint(0,numBuzzwords)
 	b = a
 	while(b==a and b!=0):
-		b = random.randint(0,bzwd)
+		b = random.randint(0,numBuzzwords)
 	if(sys.platform[:3]=='win'):
 		os.system("cls")
 	else:
 		os.system("clear")
-	print(bar[a] + " " + bar[b])
+	print(buzzwords[a] + " " + buzzwords[b])
 	print("Press enter for a new buzzword pair.")
 	pyVersion = sys.version[0]
 	if(int(pyVersion) < 3):
